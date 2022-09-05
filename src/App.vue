1e7a566e280960929e6e7a7bd3a74e885e615e26
ac <ac567e@163.com>
2022-09-05 17:57:53 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
-  <div id="app">
-    <h3>案例：折叠面板</h3>
+  <div>
     <div>
-      <div class="title">
-        <h4>芙蓉楼送辛渐</h4>
-        <span @click="spanFn = !spanFn" class="btn">
-          {{ !spanFn ? "展开" : "收起" }}
-        </span>
-      </div>
-      <div class="container" v-show="spanFn">
-        <p>寒雨连江夜入吴,</p>
-        <p>平明送客楚山孤。</p>
-        <p>洛阳亲友如相问，</p>
-        <p>一片冰心在玉壶。</p>
-      </div>
+      <span>选择所在地区：</span>
+      <input type="radio" value="中国大陆" name="area" v-model="area" />中国大陆
+      <input type="radio" value="中国香港" name="area" v-model="area" />中国香港
+    </div>
+    <div>
+      <span>手机号码：</span>
+      <input type="text" name="phone" v-model="phone" />
+    </div>
+    <div>
+      <span>年龄: </span>
+      <input type="text" name="age" v-model="age" />
+    </div>
+    <div>
+      <span>密码：</span>
+      <input type="password" name="password" v-model="password" />
+    </div>
+    <div>
+      <span>爱好：</span>
+      <input type="checkbox" v-model="hobby" value="看书" />看书
+      <input type="checkbox" v-model="hobby" value="学习" />学习
+      <input type="checkbox" v-model="hobby" value="写代码" />写代码
+      <input type="checkbox" v-model="hobby" value="游戏" />游戏
+    </div>
+    <div><input type="checkbox" v-model="isgrow" /> 记住密码</div>
+    ================================================================
+    <div>
+      我来自：{{ area }}
+      <br />
+      手机号码是：{{ phone }}
+      <br />
+      年龄是：{{ age }}
+      <br />
+      密码是：{{ password }}
+      <br />
+      爱好是：{{ hobby.join(",") }}
+      <br />
+      是否记住密码：{{ isgrow ? "是" : "否" }}
     </div>
   </div>
 </template>
@@ -22,45 +46,13 @@
 export default {
   data() {
     return {
-      spanFn: false,
+      phone: "12345678900",
+      password: "123123",
+      hobby: ["学习", "写代码", "看书"],
+      age: 123,
+      area: "中国大陆",
+      isgrow: "",
     };
   },
 };
 </script>
-
-<style lang="less">
-body {
-  background-color: #ccc;
-  #app {
-    width: 400px;
-    margin: 20px auto;
-    background-color: #fff;
-    border: 4px solid blueviolet;
-    border-radius: 1em;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-    padding: 1em 2em 2em;
-    h3 {
-      text-align: center;
-    }
-    .title {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border: 1px solid #ccc;
-      padding: 0 1em;
-    }
-    .title h4 {
-      line-height: 2;
-      margin: 0;
-    }
-    .container {
-      border: 1px solid #ccc;
-      padding: 0 1em;
-    }
-    .btn {
-      /* 鼠标改成手的形状 */
-      cursor: pointer;
-    }
-  }
-}
-</style>
