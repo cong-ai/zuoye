0b01b61a4cbd00a52a86c7b8e7bcdf9acd057e6a
ac <ac567e@163.com>
2022-09-08 08:16:30 +0800

    作业完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter("reverse", (val, q = "") => {
+  return val.split("").reverse().join(q);
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
